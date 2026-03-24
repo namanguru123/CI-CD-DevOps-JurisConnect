f7a94f1dd43fab2f1c4077ccc4abf994bf017b6e
namanguru123 <namansharma7898@gmail.com>
2026-03-24 10:13:56 +0530

    Fixed cors bug

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app=express();
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://13.51.86.216:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
